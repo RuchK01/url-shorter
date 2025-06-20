a4dfaeb9878fd18cbae7d1683436d8b8c6047218
Rucha Kalesh <ruchakalesh01@gmail.com>
2025-06-20 10:37:32 -0400

    Apply CORRECTED issues

--- app/create_link.py
+++ app/create_link.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
     body = json.loads(event['body'])
     original_url = body['url']
     short_code = ''.join(random.choices(
-        string.ascii_letters + string.digits,  # E222: Ensured exactly one space around '+'
+        string.ascii_letters + string.digits,
         k=6
     ))
 
@@ -25,10 +25,9 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps(
-            {   # E122: Indented this opening brace correctly relative to `json.dumps(`.
+            {
                 'short_url':
-                    f"{event['requestContext']['domainName']}/{short_code}" # E128: Indented 4 spaces further than 'short_url:'.
+                    f"{event['requestContext']['domainName']}/{short_code}"
             }
         )
     }
-# W292: Added a newline at the very end of the file.
