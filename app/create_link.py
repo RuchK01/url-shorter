c867827e2cf0213713a0a2b19509524026bbcdd9
Rucha Kalesh <ruchakalesh01@gmail.com>
2025-06-19 20:34:47 -0400

    Fix: Resolve SyntaxError and stylistic issues in Lambda code

--- app/create_link.py
+++ app/create_link.py
@@ -6,8 +6,7 @@ import string
 
 
 dynamodb = boto3.resource('dynamodb')
-table = 
-dynamodb.Table(os.environ['TABLE_NAME'])
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 
 def lambda_handler(event, context):
@@ -28,34 +27,5 @@ string.digits, k=6
         'body': json.dumps(
             {'short_url': 
 f"{event['requestContext']['domainName']}/{short_code}"}
-        )
-    }import 
-json import boto3
-import os
-import random
-import string
-
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-
-def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    original_url = body['url']
-    short_code = ''.join(random.choices(
-        string.ascii_letters + string.digits,
-        k=6
-    ))
-
-    table.put_item(Item={
-        'code': short_code,
-        'url': original_url
-    })
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(
-	   {'short_url':f"{event['requestContext']['domainName']}/{short_code}"}
         )
     }
